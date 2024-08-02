1e1d4494292d98fcdf13de3c4c62fd87b2510b53
Odenir Gomes <odenirdev@gmail.com>
2024-08-02 13:05:12 -0300

    fix; configured images paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["production.na01.natura.com", "files.stripe.com"],
+  },
 };
 
 export default nextConfig;
